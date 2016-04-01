792c49ba6c7d91863d756d3b38c582bb27058bcd
Mate Paiva <mmccpp@gmail.com>
2016-04-01 12:44:55 -0300

    changing the foldername

--- snippy.sh
+++ snippy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-OLDCLIPBOARD=`xsel -b`
+OLDCLIPBOARD=$(xsel -b)
 xdotool key --delay 0 ctrl+shift+Left
 xdotool key ctrl+x
 sleep 0.2s # to work reliably in Firefox
-SELECTION=`xsel -b`
-xsel -b -i < ~/.snippy/${SELECTION}
+SELECTION=$(xsel -b)
+xsel -b -i < ~/my-snippets/${SELECTION}
 xdotool key --delay 200 shift+Insert
 # xdotool key BackSpace    delete the last blank line, so we stay inline
 echo -n "${OLDCLIPBOARD}" | xclip -selection c
