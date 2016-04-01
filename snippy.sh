0a4aba743b93936c77e5dea86365362ebf2d621f
Mate Paiva <mmccpp@gmail.com>
2016-04-01 20:24:55 -0300

    Now script can work with subdirectory. YAY!

--- snippy.sh
+++ snippy.sh
@@ -4,7 +4,7 @@ xdotool key --delay 0 ctrl+shift+Left
 xdotool key ctrl+x
 sleep 0.2s # to work reliably in Firefox
 SELECTION=$(xsel -b)
-xsel -b -i < ~/my-snippets/${SELECTION}
-xdotool key --delay 200 shift+Insert
-# xdotool key BackSpace    delete the last blank line, so we stay inline
+CAMINHO=$(find -name ${SELECTION})
+xsel -b -i < ${CAMINHO}
+xdotool key --delay 50 shift+Insert
 echo -n "${OLDCLIPBOARD}" | xclip -selection c
